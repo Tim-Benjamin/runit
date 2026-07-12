2c7622c5db244c9d2934d7dcfcc97ac9425ce7fd
Benjamin Timothy <timothybenjamin7777@gmail.com>
2026-07-12 18:23:10 +0000

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@
 // In production on Vercel, use relative URL (proxied)
 // In local dev, use localhost directly
 const API_BASE = import.meta.env.DEV
-  ? 'http://localhost/runit-backend'
+  ? 'https://runit.site.je'
   : '';
 
 export default API_BASE;
